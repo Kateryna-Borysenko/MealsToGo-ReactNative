fbc472465c2800a0c9622e921efefd050f91b9ca
Kateryna Borysenko <k.borysenko.kyiv@gmail.com>
2023-01-20 14:24:25 +0200

    added theme styled-components

--- src/features/restaurants/screens/restaurant.screen.js
+++ src/features/restaurants/screens/restaurant.screen.js
@@ -1,47 +1,32 @@
 import React from "react";
 import { Searchbar } from "react-native-paper";
-import { StatusBar, StyleSheet, SafeAreaView, View } from "react-native";
+import { StatusBar, SafeAreaView } from "react-native";
 import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
 import styled from "styled-components/native";
 
 export const RestaurantsScreen = () => {
   const SafeArea = styled(SafeAreaView)`
     flex: 1;
-    margin-top: ${StatusBar.currentHeight}px;
+    ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight} px`};
   `;
 
   const SearchContainer = styled.View`
-    padding: 16px;
+    padding: ${(props) => props.theme.space[3]};
   `;
 
   const RestaurantListContainer = styled.View`
     flex: 1;
-    padding: 16px;
-    background-color: blue;
+    padding: ${(props) => props.theme.space[3]};
+    background-color: ${(props) => props.theme.colors.bg.quaternary};
   `;
   return (
-    <SafeArea style={styles.container}>
-      <SearchContainer style={styles.search}>
+    <SafeArea>
+      <SearchContainer>
         <Searchbar />
       </SearchContainer>
-      <RestaurantListContainer style={styles.list}>
+      <RestaurantListContainer>
         <RestaurantInfoCard />
       </RestaurantListContainer>
     </SafeArea>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: StatusBar.currentHeight,
-  },
-  search: {
-    padding: 16,
-  },
-  list: {
-    flex: 1,
-    padding: 16,
-    backgroundColor: "blue",
-  },
-});
