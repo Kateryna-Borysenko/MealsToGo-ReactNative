7d5c4ffb72c862d8b219d53bc1574e42cf423648
Kateryna Borysenko <k.borysenko.kyiv@gmail.com>
2023-02-06 18:04:42 +0200

    added register and login forms

--- src/services/authentication/authentication.context.js
+++ src/services/authentication/authentication.context.js
@@ -8,11 +8,29 @@ export const AuthenticationContext = createContext();
 export const AuthenticationContextProvider = ({ children }) => {
   const [isLoading, setIsLoading] = useState(false);
   const [user, setUser] = useState(null);
-  const [error, setError] = useState([]);
+  const [error, setError] = useState(null);
 
   const onLogin = (email, password) => {
     setIsLoading(true);
     loginRequest(email, password)
+      .then((u) => {
+        setUser(u);
+        setIsLoading(false);
+      })
+      .catch((e) => {
+        setIsLoading(false);
+        setError(e.toString());
+      });
+  };
+
+  const onRegister = (email, password, repeatedPassword) => {
+    if (password !== repeatedPassword) {
+      setError("Error: Passwords do not match");
+      return;
+    }
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
       .then((u) => {
         setUser(u);
         setIsLoading(false);
@@ -31,6 +49,7 @@ export const AuthenticationContextProvider = ({ children }) => {
         isLoading,
         error,
         onLogin,
+        onRegister,
       }}
     >
       {children}
